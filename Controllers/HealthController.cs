b2dbb57998aa2b6b1477aa96670e061fda969981
Raaz709 <rajchapagain98@gmail.com>
2026-08-10 16:32:31 +0545

    MySql with dapper and Swagger is connected

---
+++ Controllers/HealthController.cs
@@ -0,0 +1,31 @@
+﻿using Dapper;
+using Library_Management.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryManagementSystem.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    private readonly IDbConnectionFactory _connectionFactory;
+
+    public HealthController(IDbConnectionFactory connectionFactory)
+    {
+        _connectionFactory = connectionFactory;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> Get()
+    {
+        using var connection = _connectionFactory.CreateConnection();
+
+        var result = await connection.QuerySingleAsync<int>("SELECT 1;");
+
+        return Ok(new
+        {
+            status = "Healthy",
+            database = result == 1 ? "Connected" : "Unavailable"
+        });
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+using Library_Management.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllers();
+
+// Database
+builder.Services.AddScoped<IDbConnectionFactory, DbConnectionFactory>();
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
